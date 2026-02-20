65c55c158f609bef6f0ddc7d2c2e1198a6949138
yohann.pech <yohann.pech@epita.fr>
2026-02-20 11:25:44 +0100

    KAN-17 - fix CI : désactivation de la base de données pour le test par défaut

--- src/test/java/com/stam/api/StamApiApplicationTests.java
+++ src/test/java/com/stam/api/StamApiApplicationTests.java
@@ -2,12 +2,20 @@ package com.stam.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootTest
+// On désactive la configuration de la base de données juste pour ce test
+@SpringBootTest(classes = StamApiApplication.class)
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 class StamApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
