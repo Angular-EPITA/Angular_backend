ecdda23a2fd9d1c7d550f99b6014b1c9a3671377
yohann.pech <yohann.pech@epita.fr>
2026-02-20 11:06:12 +0100

    KAN-39 : Initialisation du socle Sprint Boot

---
+++ src/test/java/com/stam/api/StamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stam.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
